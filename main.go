97eb769a3171e860f3b6a35450ebb8151442f90f
kevin <kevin@puhanda.com>
2018-09-28 09:40:07 +0800

    build restful api with gin web framework

--- main.go
+++ main.go
@@ -1,47 +1,18 @@
-### 使用的Go语言的web框架gin-gonic构建RESTful API服务
-
-使用`Go`语言构建一个`todo`应用的API，使用的是简单快速的框架`gin-goni`框架，后端使用`mongodb`,需要安装的三方库
-
-```
-go get gopkg.in/gin-gonic/gin.v1
-go get github.com/globalsign/mgo
-```
-
-创建下面的API接口  
-
-* POST      todos/
-* GET       todos/ 
-* GET       todos/{id}
-* PUT       todos/{id}
-* DELETE    todos/{id}
-
-#### 初始化路由
-
-```
 package main
 
 import (
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/globalsign/mgo/bson"
+
+	"github.com/globalsign/mgo"
+
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	router := gin.Default()
-	v1 := router.Group("/api/v1/todos")
-	{
-		v1.POST("/", createTodo)
-		v1.GET("/", fetchAllTodo)
-		v1.GET("/:id", fetchSingleTodo)
-		v1.PUT("/:id", updateTodo)
-		v1.DELETE("/:id", deleteTodo)
-	}
-	router.Run()
-}
-
-```
-
-关于 `createTodo`等方法会在后面一一介绍，先创建一下`ToDo`相关的model  
-
-```
 const (
 	db         = "ToDo"
 	collection = "ToDoList"
@@ -52,6 +23,7 @@ var globalS *mgo.Session
 
 type (
 	todoModel struct {
+		ID        string    `bson:"_id" json:"id"`
 		Title     string    `bson:"title" json:"title"`
 		Completed int       `bson:"completed" json:"completed"`
 		CreatedAt time.Time `bson:"createdAt" json:"createdAt"`
@@ -66,17 +38,13 @@ type (
 )
 
 func init() {
-	globalS, err := mgo.Dial(host)
+	ms, err := mgo.Dial(host)
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+	globalS = ms
 }
-```
 
-#### createTodo 路由方法
-
-```
 func createTodo(c *gin.Context) {
 	completed, _ := strconv.Atoi(c.PostForm("completed"))
 	todo := todoModel{
@@ -93,13 +61,7 @@ func createTodo(c *gin.Context) {
 	c.JSON(http.StatusCreated, gin.H{"status": http.StatusCreated,
 		"message": "Todo item created successfully!", "resourceId": todo.ID})
 }
-```
 
-从`gin`的上下文获取post的数据，并保存数据到数据库，如果成功，返回对应的id
-
-#### fetchAllTodo 路由方法
-
-```
 func fetchAllTodo(c *gin.Context) {
 	var todos []todoModel
 	var _todos []transformedTodo
@@ -119,21 +81,17 @@ func fetchAllTodo(c *gin.Context) {
 		} else {
 			completed = false
 		}
-		_todos = append(_todos, transformedTodo{ID: item.ID, Title: item.Title, Completed: completed, CreatedAt: time.Now()})
+		_todos = append(_todos, transformedTodo{ID: item.ID, Title: item.Title, Completed: completed, CreatedAt: item.CreatedAt})
 	}
 	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": _todos})
 }
-```
 
-#### fetchSingleTodo 路由方法
-
-```
 func fetchSingleTodo(c *gin.Context) {
 	var todo todoModel
 	id := c.Param("id")
 	ms := globalS.Copy()
 	mc := ms.DB(db).C(collection)
-	mc.FindId(id)
+	mc.FindId(id).One(&todo)
 	if todo.ID == "" {
 		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "No todo found!"})
 		return
@@ -147,11 +105,7 @@ func fetchSingleTodo(c *gin.Context) {
 	_todo := transformedTodo{ID: todo.ID, Title: todo.Title, Completed: completed, CreatedAt: todo.CreatedAt}
 	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": _todo})
 }
-```
 
-#### updateTodo 路由方法
-
-```
 func updateTodo(c *gin.Context) {
 	var todo todoModel
 	id := c.Param("id")
@@ -166,14 +120,14 @@ func updateTodo(c *gin.Context) {
 	todo.Title = c.PostForm("title")
 	todo.Completed, _ = strconv.Atoi(c.PostForm("completed"))
 
-	mc.UpdateId(id, todo)
+	err := mc.UpdateId(id, todo)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"status": http.StatusInternalServerError, "message": "update error!"})
+		return
+	}
 	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "message": "Todo update successfully!"})
 }
-```
 
-#### deleteTodo 路由方法
-
-```
 func deleteTodo(c *gin.Context) {
 	var todo todoModel
 	id := c.Param("id")
@@ -188,22 +142,16 @@ func deleteTodo(c *gin.Context) {
 	mc.RemoveId(id)
 	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "message": "Todo deleted successfully!"})
 }
-```
 
-#### 运行测试
-
-运行  
-
-`go run main.go`  
-
-使用 `postman`测试提交数据  
-
-* createTodo
-  ![createTodo](http://wx4.sinaimg.cn/large/6d6f88d1ly1fvo44szr3sj218g0bj0td.jpg)
-* fetchSingleTodo
-  ![fetchSingleTodo](http://wx4.sinaimg.cn/large/6d6f88d1ly1fvo44vpatij218i0aiwf4.jpg)
-* deleteTodo
-  ![deleteTodo](http://wx2.sinaimg.cn/large/6d6f88d1ly1fvo44y83m2j218i08fgm2.jpg)
-
-[更多精彩内容](http://coderminer.com)  
-[译自](https://medium.com/@thedevsaddam/build-restful-api-service-in-golang-using-gin-gonic-framework-85b1a6e176f3)    
+func main() {
+	router := gin.Default()
+	v1 := router.Group("/api/v1/todos")
+	{
+		v1.POST("/", createTodo)
+		v1.GET("/", fetchAllTodo)
+		v1.GET("/:id", fetchSingleTodo)
+		v1.PUT("/:id", updateTodo)
+		v1.DELETE("/:id", deleteTodo)
+	}
+	router.Run()
+}
